0b11cb0d78a01cd356bec796becc69ab2aec1348
Kirrsi <trospe@gmx.de>
2019-02-20 19:09:39 +0100

    making the carousel work

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  root 'simple_pages#landing_page'
-  get 'simple_pages/index'
+  root 'simple_pages#index'
+  
   get 'simple_pages/about'
   get 'simple_pages/contact'
+  get 'simple_pages/landing_page'
 end
