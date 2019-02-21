f4ac0887ba7b0e16d3c74270aee72b1034f28921
Kirrsi <trospe@gmx.de>
2019-02-21 13:40:09 +0100

    put all the methods in one class on simple_pages and removed the id from show and edit

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,9 +2,7 @@ class SimplePagesController < ApplicationController
   def index
     @products = Product.limit(3)
   end
-end
 
-class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
   end
