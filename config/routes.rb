9abeed284703496120f7f9eedd611838aa0336f2
Kirrsi <trospe@gmx.de>
2019-02-23 16:35:32 +0100

    5.3 adding form and email generation

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   root 'simple_pages#index'
-  
+
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/landing_page'
+  post 'simple_pages/thank_you'
 end
