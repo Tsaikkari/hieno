832ecd73932389753007a136f10829c8dc19da98
Kirrsi <trospe@gmx.de>
2019-03-05 23:54:40 +0100

    5.8 Adding comments/review and Raty plugin

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,13 @@ class Product < ApplicationRecord
   def highest_rating_comment
     comments.rating_desc.first
   end
+
   def lowest_rating_comment
     comments.rating_desc.last
+  end
+
+  def average_rating
+    comments.average(:rating).to_f
   end
 
   def self.search(search_term)
