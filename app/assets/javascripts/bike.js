a06343f8d5528e9d9918316e34fba420a586c5cd
Kirrsi <trospe@gmx.de>
2019-03-10 13:48:37 +0100

    Rotating the bike icon

--- app/assets/javascripts/bike.js
+++ app/assets/javascripts/bike.js
@@ -5,8 +5,9 @@ var t = setInterval(move, 10);
 
 function move() {
   if (pos >= 937) {
-    clearInterval(t);
+    clearInterval(t)
   }
+
   else {
     pos += 1;
     bike.style.left = pos + "px";
