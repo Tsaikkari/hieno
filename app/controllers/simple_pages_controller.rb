c82f6c6e7d3b095822cceb8697127750659be839
Kirrsi <trospe@gmx.de>
2019-03-12 21:05:55 +0100

    6.1 adding rspec

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -6,6 +6,14 @@ class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
+
   def thank_you
     @name = params[:name]
     @email = params[:email]
--- app/models/mailers/user_mailer.rb
+++ app/models/mailers/user_mailer.rb
@@ -3,15 +3,8 @@ class UserMailer < ApplicationMailer
 
   def contact_form(email, name, message)
   @message = message
-    mail(from: email,
+    mail(from: "contact@example.com"
          to: 'trospe@gmx.de',
          subject: "A new contact form message from #{name}")
-  end
-
-  def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 end
