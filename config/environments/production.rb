1d531be3cbba92b4bfc09e7c1acef032054b8c9b
Kirrsi <trospe@gmx.de>
2019-03-18 19:52:08 +0100

    changing production configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,16 +86,16 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   # Disable Rails's static asset server (Apache or nginx will already do this)  
-config.serve_static_assets = false
+  config.serve_static_assets = false
 
-# Compress JavaScripts and CSS  
-config.assets.compress = true
+  # Compress JavaScripts and CSS  
+  config.assets.compress = true
 
-# Don't fallback to assets pipeline if a precompiled asset is missed  
-config.assets.compile = false
+  # Don't fallback to assets pipeline if a precompiled asset is missed  
+  config.assets.compile = true
 
-# Generate digests for assets URLs  
-config.assets.digest = true
+  # Generate digests for assets URLs  
+  config.assets.digest = true
 
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
