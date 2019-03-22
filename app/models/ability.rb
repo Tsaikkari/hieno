21619a5b960e8d003ad3df2e295650317792a5cd
Kirrsi <trospe@gmx.de>
2019-03-22 19:32:14 +0100

    Styling

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,13 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.present? # additional permissions for logged in users
     can :manage, User, id: user.id
-    can :manage, Comments, user_id: user.id
       if user.admin? # additional permissions for administrators
       can :manage, :all
+      end
+    
+      def initialize(comment)
+        comment ||= Comment.new
+        can :manage, Comments, user_id: user.id
       end
     end
   end
