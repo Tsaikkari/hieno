e522d9b2eca4c40d8a84109b4cf4cd592edf51f7
Kirrsi <trospe@gmx.de>
2019-03-24 13:24:12 +0100

    6.4 orrecting the initialize(user) and changing the redirection path in spec test.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,11 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.present? # additional permissions for logged in users
     can :manage, User, id: user.id
+    can :manage, Comment, user_id: user.id
+    end
       if user.admin? # additional permissions for administrators
       can :manage, :all
       end
-    
-      def initialize(comment)
-        comment ||= Comment.new
-        can :manage, Comments, user_id: user.id
-      end
     end
   end
-end
+      
