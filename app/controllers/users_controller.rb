8ab64c74a63e9a0aa6e316bcf2e28ddeff8f5ad6
Kirrsi <trospe@gmx.de>
2019-04-11 14:15:22 +0200

    protect_from_forgery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :redirect_user, only: [:edit, :destroy]
   before_action :authenticate_user!
   load_and_authorize_resource
+  protect_from_forgery
 
   def redirect_user
   redirect_to root_path
