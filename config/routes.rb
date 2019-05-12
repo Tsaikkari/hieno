60ad68373a1c25233bfa24e47dce0724f40958dd
Kirrsi <trospe@gmx.de>
2019-05-12 16:47:51 +0200

    6.13 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  mount ActionCable.server => '/cable'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :users
   resources :products do
@@ -13,4 +12,5 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   post 'simple_pages/thank_you'
   post 'payments/create'
+  mount ActionCable.server => '/cable'
 end
