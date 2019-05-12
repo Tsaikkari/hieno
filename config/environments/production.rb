ab7e5cfa950fae23b79874c13fced772252c764e
Kirrsi <trospe@gmx.de>
2019-05-12 16:35:27 +0200

    6.13 ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,4 +116,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://coralcat.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://coralcat.herokuapp.com', 'http://coralcat.herokuapp.com']
 end
