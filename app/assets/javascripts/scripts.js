a7259201baf644a08de87dfe90e1538a93bfe965
Kirrsi <trospe@gmx.de>
2019-05-22 20:21:57 +0200

    Making the payment was successful message stay longer and the text in the button on the products/new page visible

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,4 +1,4 @@
 $(document).on('turbolinks:load', function(){
-  $(".alert").delay(4000).fadeOut("ease"); 
+  $(".alert").delay(10000).fadeOut("ease"); 
   $(".notice").delay(4000).fadeOut("ease"); 
 });
