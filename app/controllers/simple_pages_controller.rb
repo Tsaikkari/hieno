c5faad5b77f3eb4aaed9e20f2c505f16ee15db6d
Kirrsi <trospe@gmx.de>
2019-05-22 20:51:27 +0200

    Removing extra deliver_now

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -10,9 +10,6 @@ class SimplePagesController < ApplicationController
     @email = params[:email]
     @message = params[:message]
     UserMailer.contact_form(@email, @name, @message).deliver_now
-    ActionMailer::Base.mail(from: @email,
-    to: 'trospe@gmx.de',
-    subject: "A new contact form message from #{@name}",
-    body: @message).deliver_now
   end
 end
+
