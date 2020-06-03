c618e8ab18d4dc3022c0eb219defa02a7987f299
Tsaikkari <trospe@gmx.de>
2020-06-03 20:51:36 +0200

    Changing image_url

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,63 +10,63 @@ products = Product.create(
         { 
             name: 'Active Bike', 
             description: 'Wonderful', 
-            image_url: '/images/activebike.jpg', 
+            image_url: 'activebike.jpg', 
             colour: 'Silver', 
             price: '80 €' 
         }, 
         { 
             name: 'Summer Bike', 
             description: 'OK Bike', 
-            image_url: '/images/summerbike.jpg', 
+            image_url: 'summerbike.jpg', 
             colour: 'White', 
             price: '50 €' 
         }, 
         { 
             name: 'Tandem Bike', 
             description: 'Awesome Bike', 
-            image_url: '/images/tandem.jpg', 
+            image_url: 'tandem.jpg', 
             colour: 'Red', 
             price: '90 €' 
         }, 
         { 
             name: "Girl/'s Bike", 
             description: 'Cute Bike', 
-            image_url: '/images/citybike.jpg', 
+            image_url: 'citybike.jpg', 
             colour: 'Pink', 
             price: '50 €' 
         }, 
         { 
             name: "Women/'s Bike", 
             description: 'Nice Bike', 
-            image_url: '/images/mybike.jpg', 
+            image_url: 'mybike.jpg', 
             colour: 'Black', 
             price: '80 €' 
         }, 
         { 
             name: 'Mountain Bike', 
             description: 'Cool Bike', 
-            image_url: '/images/mountainbike.jpg', 
+            image_url: '/mountainbike.jpg', 
             colour: 'Black', 
             price: '90 €' 
         }, 
         { 
             name: 'Finnish Bike', 
             description: 'Great Bike', 
-            image_url: '/images/jopo2.jpg', 
+            image_url: 'jopo2.jpg', 
             colour: 'Red', 
             price: '80 €' 
         }, 
         { 
             name: "Grandma/'s Bike", 
             description: 'Beautiful', 
-            image_url: '/images/grandmasbike2.jpg', 
+            image_url: 'grandmasbike2.jpg', 
             colour: 'Red', 
             price: '70 €' 
         }, 
         { 
             name: 'Street Bike', 
             description: 'Cool Bike', 
-            image_url: '/images/streetbike.jpg', 
+            image_url: 'streetbike.jpg', 
             colour: 'Orange', 
             price: '90 €' 
         }
