97cd7c69d4973da64394a7569214b72b6058a38a
Tsaikkari <trospe@gmx.de>
2020-06-03 20:58:25 +0200

    Adding products to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+products = Product.create(
+    [
+        { 
+            name: 'Active Bike', 
+            description: 'Wonderful', 
+            image_url: 'activebike.jpg', 
+            colour: 'Silver', 
+            price: '80 €' 
+        }, 
+        { 
+            name: 'Summer Bike', 
+            description: 'OK Bike', 
+            image_url: 'summerbike.jpg', 
+            colour: 'White', 
+            price: '50 €' 
+        }, 
+        { 
+            name: 'Tandem Bike', 
+            description: 'Awesome Bike', 
+            image_url: 'tandem.jpg', 
+            colour: 'Red', 
+            price: '90 €' 
+        }, 
+        { 
+            name: "Girl/'s Bike", 
+            description: 'Cute Bike', 
+            image_url: 'citybike.jpg', 
+            colour: 'Pink', 
+            price: '50 €' 
+        }, 
+        { 
+            name: "Women/'s Bike", 
+            description: 'Nice Bike', 
+            image_url: 'mybike.jpg', 
+            colour: 'Black', 
+            price: '80 €' 
+        }, 
+        { 
+            name: 'Mountain Bike', 
+            description: 'Cool Bike', 
+            image_url: 'mountainbike.jpg', 
+            colour: 'Black', 
+            price: '90 €' 
+        }, 
+        { 
+            name: 'Finnish Bike', 
+            description: 'Great Bike', 
+            image_url: 'jopo2.jpg', 
+            colour: 'Red', 
+            price: '80 €' 
+        }, 
+        { 
+            name: "Grandma/'s Bike", 
+            description: 'Beautiful', 
+            image_url: 'grandmasbike2.jpg', 
+            colour: 'Red', 
+            price: '70 €' 
+        }, 
+        { 
+            name: 'Street Bike', 
+            description: 'Cool Bike', 
+            image_url: 'streetbike.jpg', 
+            colour: 'Orange', 
+            price: '90 €' 
+        }
+    ]
+)
